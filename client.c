49396949ce2b09e3898cfdd18d35bbaf84da25dc
surjithbs17 <surjithbs17@gmail.com>
2016-09-10 01:20:24 -0600

    Server is listening and client is getting reply from server

--- client.c
+++ client.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <sys/type.h>
-#include <sys/socket.h>
+#include </usr/include/x86_64-linux-gnu/sys/types.h>
+#include <stdlib.h>
+#include </usr/include/x86_64-linux-gnu/sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
 
-#define PORT_NUM 5432
+#define PORT_NUM 5123
 #define MAX_LINE 256
 
 void main(int argc, char * argv[])
@@ -13,14 +15,16 @@ void main(int argc, char * argv[])
   struct hostent *hp;
   struct sockaddr_in sin;
   char *host;
-  char buf[MAX_LINE];
-  int s,len;
+  char buf[MAX_LINE],recv_buf[MAX_LINE];
+  int s,len,port;
 
-  if(argc==2) {
+  if(argc > 1) {
     host = argv[1];
+    port = atoi(argv[2]);
+    printf("IP Address = %s \n Port Number = %d",host,port);
   }
   else {
-    perror("Error: Formatting/ no argument");
+    perror("Error: Formatting no argument");
     exit(1);
   }
 
@@ -34,8 +38,10 @@ void main(int argc, char * argv[])
 //build address data struct
   bzero((char *)&sin,sizeof(sin));
   sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_port = htons(PORT_NUM);
+  //sin.sin_addr = INADDR_ANY;
+  sin.sin_addr.s_addr = inet_addr(host);
+  sin.sin_port = htons(port);
+
 
 //active open
   if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
@@ -43,8 +49,9 @@ void main(int argc, char * argv[])
     exit(1);
   }
 
-  if (connect.(s,(struct sockaddr *)&sin, sizeof(sin)) < 0) {
+  if (connect(s,(struct sockaddr *)&sin, sizeof(sin)) < 0) {
     perror("Error: Connect");
+    close(s);
     exit(1);
   }
 
@@ -53,6 +60,12 @@ void main(int argc, char * argv[])
     buf[MAX_LINE-1] = '\0';
     len = strlen(buf) + 1;
     send(s,buf,len,0);
+    while(len = recv(s,recv_buf,sizeof(recv_buf),0))
+    {
+      fputs(recv_buf, stdout);
+      if(strcmp(recv_buf,"Let's End it!"))
+        break;
+    }
   }
 
 }
--- server.c
+++ server.c
@@ -1,24 +1,31 @@
+
+
 #include <stdio.h>
-#include <sys/type.h>
-#include <sys/socket.h>
+#include <stdlib.h>
+#include </usr/include/x86_64-linux-gnu/sys/types.h>
+#include </usr/include/x86_64-linux-gnu/sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define PORT_NUM 5432
+#define PORT_NUM 5123
 #define MAX_PENDING 5
 #define MAX_LINE 256
 
-void main()
+void main(int argc, char *argv[])
 {
   struct sockaddr_in sin;
-  char buf[MAX_LINE];
+  char buf[MAX_LINE],parsed_buf[MAX_LINE];
   int len;
   int s,new_s;
+  FILE *ls_fp;
+  char parsed_char;
 
   //Address data structure
   bzero((char *)&sin,sizeof(sin));
   sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = INADDR_ANY;
+  sin.sin_addr.s_addr = inet_addr("127.0.1.1");
   sin.sin_port = htons(PORT_NUM);
 
   //setup passive open
@@ -27,21 +34,46 @@ void main()
     exit(1);
   }
 
-  if((bind(s.(struct sockaddr *)&sin, sizeof(sin))) < 0) {
+  if((bind(s,(struct sockaddr *)&sin, sizeof(sin))) < 0) {
     perror("Error: Bind");
     exit(1);
   }
 
-  listen(s.MAX_PENDING);
+  printf("Going to listen and %d",PORT_NUM);
 
+  listen(s,MAX_PENDING);
+  printf("\nListening ");
   while (1) {
-    if((new_s = accept(s.(struct sockaddr *)&sin, &len)) < 0 ){
+
+    if((new_s = accept(s,(struct sockaddr *)&sin, &len)) < 0 ){
       perror("Error: Accept");
       exit(1);
     }
-    while (len = recv(new_s, buf, sizeof(buf), 0)) {
-      fputs(buf, stdout);
+    printf("Connection Accepted\n");
+
+    while (len = recv(new_s, buf, sizeof(buf), 0)) 
+      if(strcmp(buf,"ls"))
+      {
+      		printf("Inside LS\n");
+      		system("ls -l > ls_file");
+      		ls_fp = fopen("ls_file","r");
+      		int i=0;
+      		while(parsed_char = getc(ls_fp) != EOF)
+      		{
+      			parsed_buf[i] = parsed_char;
+      			if(i==254)
+      			{
+      				parsed_buf[i++] = '\0';
+      				send(new_s, parsed_buf, (strlen(parsed_buf) + 1),0);
+      				i = -1;
+      			}
+      			i++;
+      		}
+
+
+
       }
+
     close(new_s);
   }
 
